9b93ae0a35481bb9aa105d97c497f233131b1c28
jamiebradford123 <jamiebradford123@hotmail.com>
2023-01-24 14:21:42 +0000

    add admin to blog

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
